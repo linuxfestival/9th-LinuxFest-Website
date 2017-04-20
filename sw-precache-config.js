dec3297dda2e023cb404d74162efee50152476a0
Parham Alvani <parham.alvani@gmail.com>
2017-04-20 17:41:59 +0430

    Project basis were added.

---
+++ sw-precache-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/src/**/*',
+    '/scripts/**/*',
+    '/images/**/*',
+    '/data/**/*'
+  ],
+  navigateFallback: '/index.html',
+  navigateFallbackWhitelist: [ /^\/[^\_]+\// ]
+};
